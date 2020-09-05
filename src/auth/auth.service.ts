97a4524f418f9dc2c629ed36a21f3c2051740680
Rafael Zanella <rafaels.zanella@gmail.com>
2020-09-05 01:46:58 -0300

    feat: implement jwt functionality.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService
+  ) {}
 
   /*
     WARNING
@@ -17,5 +21,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    }
   }
 }
