efb1be11e3e032ae0e11437f194a8ef16a15ca7b
Rafael Zanella <rafaels.zanella@gmail.com>
2020-09-05 02:19:14 -0300

    feat: implement protected route and jwt strategy guards.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,9 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
@@ -12,10 +13,10 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '6000s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService]
 })
 export class AuthModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { jwtConstants } from "./constants";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  };
+
+  async validate(payload: any) {
+    return { userID: payload.sub, username: payload.username}
+  }
+}
+
+// https://github.com/mikenicholson/passport-jwt#extracting-the-jwt-from-the-request
